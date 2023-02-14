b99cb977e7d74ad669014e855c98790feef388a3
Sergey Pavlov <self@sergeypavlov.dev>
2023-02-14 23:43:28 +0300

    Add method for getting file reader to S3 client interface

--- gwserver/core/s3/s3.py
+++ gwserver/core/s3/s3.py
@@ -1,8 +1,14 @@
 import io
+from typing import Protocol
 
 from boto3 import session
 
 from gwserver.core import config
+
+
+class Reader(Protocol):
+    def read(self) -> bytes:
+        ...
 
 
 class S3:
@@ -17,11 +23,14 @@ class S3:
             aws_secret_access_key=config.S3_SECRET,
         )
 
-    def download(self, key: str) -> bytes:
+    def reader(self, key: str) -> Reader:
         response = self.client.get_object(Bucket=self.bucket, Key=key)
-        data: bytes = response["Body"].read()
+        body: Reader = response["Body"]
 
-        return data
+        return body
+
+    def download(self, key: str) -> bytes:
+        return self.reader(key).read()
 
     def upload(self, key: str, content: bytes) -> None:
         self.client.upload_fileobj(io.BytesIO(content), self.bucket, key)
